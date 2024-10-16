c92d93acb541f46cd8ecc8886caa698718c85b97
Islam <islambassiem@gmail.com>
2024-10-16 09:50:36 +0300

    adding autofocus to the input

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,7 +66,7 @@
                 <div class="mb-5">
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                         </label>
-                    <input type="email" id="email" name="email"
+                    <input type="email" id="email" name="email" autofocus
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Enter Your Email Here" required />
                     <p class="italic mb-3 text-sm mt-1">
